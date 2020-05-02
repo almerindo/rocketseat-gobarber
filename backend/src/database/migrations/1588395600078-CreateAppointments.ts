aa02a0beabfa32c515bc3b6d0c7b5e370b814a64
almerindo <almerindo.rehem@gmail.com>
2020-05-02 02:50:33 -0300

    feat: add Repository TypeORM

--- backend/src/database/migrations/1588395600078-CreateAppointments.ts
+++ backend/src/database/migrations/1588395600078-CreateAppointments.ts
@@ -11,6 +11,7 @@ class CreateAppointments1588395600078 implements MigrationInterface {
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
