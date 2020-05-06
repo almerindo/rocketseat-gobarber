0e9375e1446959c339c831ec6c2124dbbd1ae035
almerindo <almerindo.rehem@gmail.com>
2020-05-05 22:30:22 -0300

    feat: add login page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalStryle from './styles/global';
+
+import SignIn from './pages/SignIn';
+
+const App: React.FC = () => (
+  <>
+    <SignIn />
+    <GlobalStryle />
+  </>
+);
+
+export default App;
